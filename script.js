6147b372117a7a2223f1241d309246b13e73a27d
Nicholas Sotter <112256391+devsotter@users.noreply.github.com>
2022-10-01 00:12:09 -0300

    add scroll reveal

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+/*Dark Mode*/
 
 const changeThemeBtn = document.querySelector("#change-theme");
-
 
 function toggleDarkMode() {
   document.body.classList.toggle("dark");
@@ -19,7 +19,6 @@ loadTheme();
 changeThemeBtn.addEventListener("change", function () {
   toggleDarkMode();
 
-
   localStorage.removeItem("dark");
 
   if (document.body.classList.contains("dark")) {
@@ -27,10 +26,21 @@ changeThemeBtn.addEventListener("change", function () {
   }
 });
 
+/*Menu Hamburger*/
+
 const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu")
+const navMenu = document.querySelector(".nav-menu");
 
 hamburguer.addEventListener("click", () => {
-  hamburguer.classList.toggle('active');
-  navMenu.classList.toggle('active');
-})
+  hamburguer.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+/*Scroll Reveal*/
+
+window.sr = ScrollReveal({ reset: true });
+ScrollReveal().reveal(".area-3", { delay: 200 });
+ScrollReveal().reveal(".area-2", { delay: 200 });
+ScrollReveal().reveal(".area-4", { delay: 200 });
+ScrollReveal().reveal(".area-5", { delay: 200 });
+ScrollReveal().reveal(".area-1", { delay: 200 });
