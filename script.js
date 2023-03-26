61833d53befa35d1fcf0e648550b6609e5f77072
Nicholas Sotter <devsotter@gmail.com>
2023-03-26 09:46:42 -0300

    modal e bugs

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ hamburguer.addEventListener("click", () => {
   hamburguer.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
+
+
+/*modal*/
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
